46279ef502e8ccde84cfec860f909473f070eb32
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-09-16 15:50:48 +0200

    Creating configure script.

--- configure.py
+++ configure.py
@@ -6,15 +6,31 @@ import json
 import os.path
 import time
 from socket import gethostname
-
+from pathlib import Path
 
 CONFIG_FILE = "/etc/radiation-benchmarks.conf"
-DISABLE_CONSOLE_LOGGING = False
-BATCH_SIZE = 1
 ITERATIONS = int(1e12)
+TEST_SAMPLES = 128 * 100
 DOWNLOAD_MODELS = False
 
-DNN_MODELS = {}
+DNN_MODELS = {
+    # Diehardnet
+    # Cifar 100
+    "c100_res44_test_01_bn-relu_base.yaml",
+    "c100_res44_test_02_bn-relu6.yaml",
+    "c100_res44_test_02_relu6-bn.yaml",
+    "c100_res44_test_02_bn-relu6_base.yaml",
+    "c100_res44_test_02_gelu6_nans.yaml",
+    # Cifar 10
+    "c10_res44_test_02_bn-relu6_base.yaml",
+    "c10_res44_test_01_bn-relu_base.yaml",
+    "c10_res44_test_02_bn-relu6.yaml",
+    "c10_res44_test_02_relu6-bn.yaml",
+    "c10_res44_test_02_gelu6_nans.yaml",
+    # Mobile net v2
+    "cifar100_mobilenetv2_x1_4.yaml",
+    "cifar10_mobilenetv2_x1_4.yaml"
+}
 TEST_JSONS = False
 
 
@@ -27,6 +43,7 @@ def main():
         raise IOError("Configuration setup error: " + str(e))
 
     hostname = gethostname()
+    home = str(Path.home())
     jsons_path = f"data/{hostname}_jsons"
     if os.path.isdir(jsons_path) is False:
         os.mkdir(jsons_path)
@@ -36,41 +53,33 @@ def main():
         os.system("./download_models.py")
 
     current_directory = os.getcwd()
-    for dnn_model, config_vals in DNN_MODELS.items():
-        dnn_type = config_vals["type"]
-        dataset = config_vals["dataset"]
+    script_name = "main.py"
+    for dnn_model in DNN_MODELS:
         # Default filename will build the other names
-        default_file_name = f"{hostname}_config_{dnn_model}_"
-        default_file_name += f"{dnn_type}_{dataset}_batch_size_{BATCH_SIZE}"
+        default_file_name = dnn_model.replace(".yaml", "")
         json_file_name = f"{jsons_path}/{default_file_name}.json"
-        file_ext = ".pt"
-        gold_path = f"{current_directory}/data/{default_file_name}" + file_ext
-
-        script_name = "main.py"
-        dataset_img_list = f"{current_directory}/data/{dataset}_img_list.txt"
+        data_dir = f"{current_directory}/data"
+        gold_path = f"{data_dir}/{default_file_name}.pt"
+        checkpoint_dir = f"{current_directory}/configurations"
 
         parameters = [
-            f"{current_directory}/{script_name} ",
-            f"--model {dnn_model}",
-            f"--imglist {dataset_img_list}",
-            "--precision fp32",
+            f"{current_directory}/{script_name}",
             f"--iterations {ITERATIONS}",
-            f"--batchsize {BATCH_SIZE}",
-            "--disableconsolelog",
+            f"--testsamples {TEST_SAMPLES}",
+            f"--config {dnn_model}",
+            f"--checkpointdir {checkpoint_dir}",
+            f"--datadir {data_dir}",
             f"--goldpath {gold_path}",
         ]
-
-        generate_parameters = parameters + ["--generate"]
-        generate_parameters.remove("--disableconsolelog")
-        generate_cmd = " ".join(generate_parameters)
-
-        exec_cmd = " ".join(parameters)
+        execute_parameters = parameters + ["--disableconsolelog"]
         command_list = [{
             "killcmd": f"pkill -9 -f {script_name}",
-            "exec": exec_cmd,
+            "exec": " ".join(execute_parameters),
             "codename": script_name,
-            "header": " ".join(parameters)
+            "header": " ".join(execute_parameters)
         }]
+
+        generate_cmd = " ".join(parameters + ["--generate"])
         # dump json
         with open(json_file_name, "w") as json_fp:
             json.dump(obj=command_list, fp=json_fp, indent=4)
@@ -80,8 +89,7 @@ def main():
             raise OSError(f"Could not execute command {generate_cmd}")
 
     print("Json creation and golden generation finished")
-    print(f"You may run: scp -r {jsons_path} carol@{server_ip}:"
-          f"/home/carol/radiation-setup/radiation-setup/machines_cfgs/")
+    print(f"You may run: scp -r {jsons_path} carol@{server_ip}:{home}/radiation-setup/machines_cfgs/")
 
 
 def test_all_jsons(timeout=30):
